171fa1d5454a614d1c18b62019793ef795999020
Hyeonu Choi <renewal1990@gmail.com>
2023-12-14 14:58:26 +0900

    Empty 프로젝트 업로드

---
+++ Source/MyLyraClone/MyLyraClone.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyLyraClone.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyLyraClone, "MyLyraClone" );
---
+++ Source/MyLyraClone/MyLyraCloneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyLyraCloneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYLYRACLONE_API AMyLyraCloneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
