2accf51201d9d75d7757bfc1bbb33a19cb926738
Hyeonu Choi <renewal1990@gmail.com>
2023-12-26 16:20:39 +0900

    06 Module

--- Source/MyLyraClone/MyLyraClone.cpp
+++ Source/MyLyraClone/MyLyraClone.cpp
@@ -3,4 +3,24 @@
 #include "MyLyraClone.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyLyraClone, "MyLyraClone" );
+/**
+ * FMyLyraCloneGameModule
+ */
+class FMyLyraCloneGameModule : public FDefaultGameModuleImpl
+{
+	virtual void StartupModule() override;
+
+	virtual void ShutdownModule() override;
+};
+
+void FMyLyraCloneGameModule::StartupModule()
+{
+	UE_LOG(LogTemp, Warning, TEXT("StartupModule")); // 게임 시작 시 모듈 init 될 때 호출
+}
+
+void FMyLyraCloneGameModule::ShutdownModule()
+{
+	UE_LOG(LogTemp, Warning, TEXT("ShutdownModule")); // 게임 종료 시 모듈 deinit 될 때 호출
+}
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FMyLyraCloneGameModule, MyLyraClone, "MyLyraClone");
